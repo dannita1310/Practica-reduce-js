1d4d3bfb5916f41ab23c4d06f1eeba25724168e3
Axl Kadir Ramirez Rodriguez <akramirezr@gmail.com>
2023-08-10 13:03:46 -0600

    Practice

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function reduce() {
-  let num = [1, 2, 3, 4, 5, 6, 7];
-}
+const numeros = [1, 2, 3, 4, 5];
+
+const sumaTotal = numeros.reduce(
+  (acumulador, numero) => acumulador + numero,
+  0
+);
+
+console.log("La suma total es:", sumaTotal);
