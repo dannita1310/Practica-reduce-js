6c9bd82fa7ae8326d0f0a3c4e5e0d54ca3db9600
dannita1310 <gutierrezdanna119@gmail.com>
2023-08-10 12:58:51 -0600

    mi branch

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function reduce() {
-  let num = [1, 2, 3, 4, 5, 6, 7];
-}
+const listaDeNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const resultado = listaDeNumeros.reduce((acc, cv) => {
+  return acc + cv;
+}, 0);
+
+console.log(resultado);
